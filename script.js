1b3a305f4a249a31860c4e468d304eceaeba2951
katleene <webintosh@yahoo.it>
2022-09-28 16:17:36 +0200

    Implement the Drawing Modes

--- script.js
+++ script.js
@@ -16,33 +16,104 @@ function makeGrid(numOfCellsPerSide) {
 		}
 	}
 
-	document.querySelectorAll('.cell').forEach((cell) =>
-		cell.addEventListener('mouseover', (e) => {
+	document
+		.querySelectorAll('.cell')
+		.forEach((cell) => cell.addEventListener('mouseover', drawModeCb));
+
+	countOfCells = numOfCellsPerSide;
+}
+
+function drawModeCb(e) {
+	switch (drawingMode) {
+		case 1:
 			e.target.style.backgroundColor = '#000';
-		})
+			break;
+
+		case 2:
+			e.target.style.backgroundColor = random_rgba();
+			break;
+
+		case 3:
+			// Get the current background-color value
+			let bgColor = getComputedStyle(e.target).getPropertyValue(
+				'background-color'
+			);
+
+			// Get all color components (alpha may not be there if = 1)
+			const parts = bgColor.match(/[\d.]+/g);
+
+			// If alpha is not there, add it
+			if (parts.length === 3) parts.push(1);
+
+			// Modify alpha adding 20% more
+			parts[3] = Math.min(1, Math.max(0, parseFloat(parts[3]) + 0.2));
+
+			// Apply new value
+			e.target.style.backgroundColor = `rgba(${parts.join(',')})`;
+			break;
+
+		default:
+			break;
+	}
+}
+
+function random_rgba() {
+	var o = Math.round,
+		r = Math.random,
+		s = 255;
+	return (
+		'rgba(' +
+		o(r() * s) +
+		',' +
+		o(r() * s) +
+		',' +
+		o(r() * s) +
+		',' +
+		r().toFixed(1) +
+		')'
 	);
 }
 
 function promptUserForCountOfCells() {
-	let countOfCells = prompt('Enter the count of cells per side.');
+	let count = prompt('Enter the count of cells per side.');
 
-	countOfCells = Number(countOfCells);
+	count = Number(count);
 
-	if (countOfCells == 0 || !Number.isFinite(countOfCells)) {
+	if (count == 0 || !Number.isFinite(count)) {
 		alert('The value you entered is not a positive number.');
 		promptUserForCountOfCells();
 		return;
 	}
 
-	if (countOfCells > 100) {
+	if (count > 100) {
 		alert("The count of cells can't be grater than 100.");
 		promptUserForCountOfCells();
 		return;
 	}
+
+	makeGrid(count);
 }
 
-makeGrid(100);
+function setDrawingMode(e) {
+	const radioButtons = document.querySelectorAll('input[type="radio"]');
+
+	for (const radioButton of radioButtons) {
+		if (radioButton.checked) {
+			drawingMode = radioButton.value * 1;
+			break;
+		}
+	}
+}
+
+let drawingMode = 1;
+let countOfCells = 100;
+
+makeGrid(countOfCells);
 
 document
 	.querySelector('#askCountOfCells')
 	.addEventListener('click', promptUserForCountOfCells);
+
+document
+	.querySelectorAll('input[type="radio"]')
+	.forEach((radio) => addEventListener('click', setDrawingMode));
